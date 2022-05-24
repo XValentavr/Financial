1f0b6f9047775312e5fe10d10f64ffe0a66626c4
Valentyn <iwilly17@gmail.com>
2022-05-24 12:24:54 +0300

    added new wallet money visualization

--- financial/static/js/purse.js
+++ financial/static/js/purse.js
@@ -13,10 +13,10 @@ function GetData(data) {
         let banker = data[i];
         let json_departments = {
             'bank': banker['account'],
-            'usd': parseFloat(banker['USD']).toFixed(1).toString()+' USD',
-            'eur': parseFloat(banker['EUR']).toFixed(1).toString()+' EUR',
-            'uah': parseFloat(banker['UAH']).toFixed(1).toString()+' UAH',
-            'pln': parseFloat(banker['PLN']).toFixed(1).toString()+' PLN',
+            'usd': parseFloat(banker['USD']).toFixed(1).toString() + ' USD',
+            'eur': parseFloat(banker['EUR']).toFixed(1).toString() + ' EUR',
+            'uah': parseFloat(banker['UAH']).toFixed(1).toString() + ' UAH',
+            'pln': parseFloat(banker['PLN']).toFixed(1).toString() + ' PLN',
         }
         information.push(json_departments)
     }
@@ -34,15 +34,20 @@ function CreateTable(data) {
         let element = data[i];
         let row = table.insertRow();
         let cell = row.insertCell();
+        cell.classList.add('place')
+        let cell1 = row.insertCell();
+        cell1.classList.add('amount')
         let a = document.createElement("a");
         a.setAttribute("href", `/walletinfo/${element[keys[0]]}`);
         let text = document.createTextNode(element[keys[0]]);
         a.appendChild(text);
         cell.appendChild(a);
         for (let j = 1; j < keys.length; j++) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[keys[j]]);
-            cell.appendChild(text);
+            let amount = document.createElement("div");
+            if (!element[keys[j]].includes('NaN')) {
+                amount.innerText = element[keys[j]]
+                cell1.appendChild(amount)
+            }
         }
     }
 }
